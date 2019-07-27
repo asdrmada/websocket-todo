58557ede7606d1b2961662f7bd8ef7301434c407
asdrmada <42623227+asdrmada@users.noreply.github.com>
2019-07-27 19:04:51 +0100

    Socket.IO connected too application

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require('express'),
       app     = express(),
-      http    = require('http').createServer(app),
-      io      = require('socket.io')(http);
+      server  = require('http').createServer(app),
+      io      = require('socket.io').listen(server);
+      socket  = require('socket.io-client')('http://localhost');
+
+io.on('connection', (socket) => {
+    console.log('Socket Live!')
+    
+    socket.on('connect', () => {
+        console.log('client connected')
+    });
+    socket.on('event', (data) => {
+        console.log('List updated!')
+    })
+    socket.off('disconnect', () => {
+        console.log('Client Disconnected')
+    })
+});
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
 });
 
-io.on('connection', (socket) => {
-    console.log('a user has conencted!')
-});
-
-app.listen(process.env.PORT || 3000, process.env.IP, () => {
+server.listen(process.env.PORT || 3000, process.env.IP, () => {
     console.log("The server.....it's alive!!!");
    });
